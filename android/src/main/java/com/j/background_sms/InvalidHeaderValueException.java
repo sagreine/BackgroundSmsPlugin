6ff4805d83b6c17432ab20fac4b7c67638e05e36
sagreine <sagreine@gmail.com>
2024-08-22 22:15:49 -0400

    add invalidheader

---
+++ android/src/main/java/com/j/background_sms/InvalidHeaderValueException.java
@@ -0,0 +1,18 @@
+
+public class InvalidHeaderValueException extends MmsException {
+    private static final long serialVersionUID = -2053384496042052262L;
+    /**
+     * Constructs an InvalidHeaderValueException with no detailed message.
+     */
+    public InvalidHeaderValueException() {
+        super();
+    }
+    /**
+     * Constructs an InvalidHeaderValueException with the specified detailed message.
+     *
+     * @param message the detailed message.
+     */
+    public InvalidHeaderValueException(String message) {
+        super(message);
+    }
+}
