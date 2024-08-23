b4fd8be8f3ecaa6ff95937aef659b63b04f765fd
sagreine <sagreine@gmail.com>
2024-08-23 16:19:35 -0400

    Update SendReq.java

--- android/src/main/java/com/j/background_sms/SendReq.java
+++ android/src/main/java/com/j/background_sms/SendReq.java
@@ -4,15 +4,18 @@ import android.util.Log;
 
 public class SendReq extends MultimediaMessagePdu {
     private static final String TAG = "SendReq";
+    private static final int P_CONTENT_TYPE_IN       = 0x91;
     public SendReq() {
         super();
             setMessageType(PduHeaders.MESSAGE_TYPE_SEND_REQ);
             setMmsVersion(PduHeaders.CURRENT_MMS_VERSION);
             // FIXME: Content-type must be decided according to whether
             // SMIL part present.
-            setContentType("application/vnd.wap.multipart.related".getBytes());
+            //setContentType("application/vnd.wap.multipart.related".getBytes());
+         mPartHeader.put(P_CONTENT_TYPE_IN, "application/vnd.wap.multipart.related".getBytes());
+     
             setFrom(new EncodedStringValue(PduHeaders.FROM_INSERT_ADDRESS_TOKEN_STR.getBytes()));
-            setTransactionId(generateTransactionId());
+            //setTransactionId(generateTransactionId());
     }
       public void AddTo(EncodedStringValue[] value) {
         mPduHeaders.setEncodedStringValues(value, PduHeaders.TO);
