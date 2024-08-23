685edefdbd3b6baec220cda0c5122ef61db08b15
sagreine <sagreine@gmail.com>
2024-08-22 22:08:09 -0400

    bring in source

---
+++ android/src/main/java/com/j/background_sms/SendReq.java
@@ -0,0 +1,16 @@
+public class SendReq extends MultimediaMessagePdu {
+    private static final String TAG = "SendReq";
+    public SendReq() {
+        super();
+            setMessageType(PduHeaders.MESSAGE_TYPE_SEND_REQ);
+            setMmsVersion(PduHeaders.CURRENT_MMS_VERSION);
+            // FIXME: Content-type must be decided according to whether
+            // SMIL part present.
+            setContentType("application/vnd.wap.multipart.related".getBytes());
+            setFrom(new EncodedStringValue(PduHeaders.FROM_INSERT_ADDRESS_TOKEN_STR.getBytes()));
+            setTransactionId(generateTransactionId());
+    }
+      public void AddTo(EncodedStringValue[] value) {
+        mPduHeaders.setEncodedStringValues(value, PduHeaders.TO);
+    }
+}
